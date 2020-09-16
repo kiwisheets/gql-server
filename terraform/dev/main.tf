930efa3ea11ef56014f76e8f4451101477f967e0
Max Troughear <max@maxtroughear.dev>
2020-09-17 08:36:14 +1200

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -12,7 +12,7 @@ provider "nomad" {
   ca_file = "nomad-ca.pem"
 }
 
-resource "nomad_job" "frontend" {
+resource "nomad_job" "gql-server" {
   jobspec = file("${path.module}/jobs/gqlserver.hcl")
   detach  = false
 }
