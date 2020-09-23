bab6f69ae1ca7ddd3374efca5324616901c2cb2b
Max Troughear <max@maxtroughear.dev>
2020-09-24 00:19:02 +1200

    utilise more efficient dataloading for permissions checking

--- graphql/directive/directives.go
+++ graphql/directive/directives.go
@@ -12,7 +12,7 @@ import (
 	"git.maxtroughear.dev/max.troughear/digital-timesheet/go-server/graphql/generated"
 	"git.maxtroughear.dev/max.troughear/digital-timesheet/go-server/util"
 	"github.com/99designs/gqlgen/graphql"
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 )
 
 // Register function registers all directives
@@ -39,46 +39,71 @@ func Register(db *gorm.DB, cfg *util.ServerConfig) generated.DirectiveRoot {
 			}
 
 			// should probably optimise this directive as it will be called on most requests
-			roles, err := dataloader.For(ctx).RolesByUserID.Load(auth.For(ctx).User.IDint())
+			// roles, err := dataloader.For(ctx).RolesByUserID.Load(auth.For(ctx).User.IDint())
+
+			permissions, err := dataloader.For(ctx).PermissionsByUserID.Load(auth.For(ctx).User.IDint())
 
 			if err != nil {
 				return nil, err
 			}
 
-			for _, r := range roles {
-				if r.CheckPermission(perm) {
-					// permission passed
+			for _, p := range permissions {
+				if p.CheckPermissionString(perm) {
 					return next(ctx)
 				}
 			}
 
+			// for _, r := range roles {
+			// 	if r.CheckPermission(perm) {
+			// 		// permission passed
+			// 		return next(ctx)
+			// 	}
+			// }
+
 			return nil, fmt.Errorf("not authorised")
 		},
-		HasPerms: func(ctx context.Context, obj interface{}, next graphql.Resolver, perms []string) (res interface{}, err error) {
+		HasPerms: func(ctx context.Context, obj interface{}, next graphql.Resolver, requestedPerms []string) (res interface{}, err error) {
 			if auth.For(ctx).User == nil {
 				return nil, fmt.Errorf("not logged in")
 			}
 
-			permsPassed := make([]bool, len(perms))
+			permsPassed := make([]bool, len(requestedPerms))
 
-			// should probably optimise this directive as it will be called on most requests
-			roles, err := dataloader.For(ctx).RolesByUserID.Load(auth.For(ctx).User.IDint())
+			permissions, err := dataloader.For(ctx).PermissionsByUserID.Load(auth.For(ctx).User.IDint())
 
 			if err != nil {
 				return nil, err
 			}
 
-			for _, r := range roles {
-				for i, p := range perms {
+			for _, userPerm := range permissions {
+				for i, requestedPerm := range requestedPerms {
 					if permsPassed[i] {
 						continue
 					}
-					if r.CheckPermission(p) {
-						// permission passed
+					if userPerm.CheckPermissionString(requestedPerm) {
 						permsPassed[i] = true
 					}
 				}
 			}
+
+			// // should probably optimise this directive as it will be called on most requests
+			// roles, err := dataloader.For(ctx).RolesByUserID.Load(auth.For(ctx).User.IDint())
+
+			// if err != nil {
+			// 	return nil, err
+			// }
+
+			// for _, r := range roles {
+			// 	for i, p := range perms {
+			// 		if permsPassed[i] {
+			// 			continue
+			// 		}
+			// 		if r.CheckPermission(p) {
+			// 			// permission passed
+			// 			permsPassed[i] = true
+			// 		}
+			// 	}
+			// }
 
 			for _, p := range permsPassed {
 				if !p {
