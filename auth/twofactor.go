c56474772e6fb6a0f05a1a3a412b28faafdfb8c7
Max Troughear <max@maxtroughear.dev>
2020-09-24 00:18:29 +1200

    update twofactor in auth package

--- auth/twofactor.go
+++ auth/twofactor.go
@@ -1,16 +1,50 @@
 package auth
 
 import (
+	"errors"
 	"fmt"
+	"log"
 
 	"git.maxtroughear.dev/max.troughear/digital-timesheet/go-server/orm/model"
 	"git.maxtroughear.dev/max.troughear/digital-timesheet/go-server/util"
-	"github.com/jinzhu/gorm"
 	"github.com/pquerna/otp/totp"
+	"gorm.io/gorm"
 )
+
+func GetTwoFactor(db *gorm.DB, u *model.User) (*model.TwoFactor, error) {
+	var twoFactor model.TwoFactor
+	if err := db.Model(&u).Association("TwoFactor").Find(&twoFactor); err != nil {
+		log.Println(err.Error())
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	if twoFactor.Secret == "" {
+		return nil, nil
+	}
+
+	return &twoFactor, nil
+}
+
+func IsTwoFactorEnabled(db *gorm.DB, u *model.User) (bool, error) {
+	if db.Model(&u).Association("TwoFactor").Count() != 0 {
+		return true, nil
+	}
+	return false, nil
+}
 
 // EnableTwoFactor will enable 2FA for the user passed to it if the token validates against the secret
 func EnableTwoFactor(db *gorm.DB, u *model.User, secret string, token string) ([]string, error) {
+	{
+		twoFactor, _ := GetTwoFactor(db, u)
+		if twoFactor != nil {
+			return nil, fmt.Errorf("2FA is already enabled")
+		}
+	}
+
 	if !totp.Validate(token, secret) {
 		return nil, fmt.Errorf("Invalid 2FA code")
 	}
@@ -30,12 +64,10 @@ func DisableTwoFactor(db *gorm.DB, u *model.User, password string) (bool, error)
 		return false, fmt.Errorf("Password incorrect")
 	}
 
-	var twoFactor model.TwoFactor
-	if err := db.Model(&u).Related(&twoFactor).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return true, fmt.Errorf("2FA is not enabled")
-		}
-		return false, err
+	twoFactor, err := GetTwoFactor(db, u)
+
+	if twoFactor == nil || err != nil {
+		return true, err
 	}
 
 	if err := db.Delete(&twoFactor).Error; err != nil {
@@ -46,7 +78,7 @@ func DisableTwoFactor(db *gorm.DB, u *model.User, password string) (bool, error)
 }
 
 func VerifyTwoFactor(t *model.TwoFactor, token string) bool {
-	// compare token to backup key
+	// compare token to backup keys
 	for _, k := range t.BackupKeys {
 		if token == k {
 			return true
@@ -61,7 +93,7 @@ func GetBackupKeys(db *gorm.DB, authCtx authContext) ([]string, error) {
 		return nil, fmt.Errorf("Login required")
 	}
 	var twoFactor model.TwoFactor
-	if err := db.Model(&authCtx.User).Related(&twoFactor).Error; err != nil {
+	if err := db.Model(&authCtx.User).Association("TwoFactor").Find(&twoFactor); err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return nil, fmt.Errorf("2FA is not enabled")
 		}
