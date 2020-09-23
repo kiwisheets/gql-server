49b813d4ffc4de445588d0f7ca2d2f6832bc5616
Max Troughear <max@maxtroughear.dev>
2020-09-24 00:16:15 +1200

    use enum generation for permission subjects

--- orm/model/permission/subject/subject.go
+++ orm/model/permission/subject/subject.go
@@ -1,51 +1,31 @@
+//go:generate go-enum -f=$GOFILE --marshal
+
 package subject
 
-import (
-	"database/sql/driver"
-	"fmt"
-)
+// Subject is an enumeration of permission subject values
+/*
+ENUM(
+None
+Any
 
+Me
+
+User
+Users
+UserContact
+OtherUser
+OtherUsers
+
+Company
+OtherCompany
+OtherCompanies
+
+Client
+Clients
+ClientContact
+
+Contact
+Contacts
+)
+*/
 type Subject int64
-
-const (
-	None Subject = iota
-	Any
-	Me
-	User
-	Users
-	Company
-)
-
-var subjectStrings = [...]string{
-	"",
-	"*",
-	"Me",
-	"User",
-	"Users",
-	"Company",
-}
-
-// Scan converts int64 value to OperationType
-func (s *Subject) Scan(value interface{}) error {
-	*s = Subject(value.(int64))
-	return nil
-}
-
-// Value returns the OperationType as an int
-func (s Subject) Value() (driver.Value, error) {
-	return int64(s), nil
-}
-
-func (s Subject) String() string {
-	return subjectStrings[s]
-}
-
-func (s *Subject) FromString(value string) error {
-	for i, subjectString := range subjectStrings {
-		if value == subjectString {
-			*s = Subject(int64(i))
-			return nil
-		}
-	}
-	return fmt.Errorf("invalid subject string")
-}
